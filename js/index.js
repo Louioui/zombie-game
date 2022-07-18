14e8007c768c1787e6bdfa9941502c11bb41bb1a
luis-pacheco <luispacheco13a@gmail.com>
2022-07-18 05:14:09 -0700

    imported

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import resize from "./libary/resize"
 import { width, height } from "./config"
 import Player from "../characters/player"
+import Zombie from "./characters/zombie"
+import Bullet from "./characters/bullet"
 
 const canvas = document.getElementById("zombieGame")
 const container = canvas.getContext("2d")
@@ -20,4 +22,26 @@ const container = canvas.getContext("2d")
    let mouse = pointer(canvas, e)
    player.rotate(mouse)
  })
- 
+
+ const update = (frame) => {
+    ctx.clearRect(0, 0, width, height)
+  
+    bullets.forEach(bullet => {
+      bullet.update(bullets, zombies)
+      bullet.render(char)
+    })
+  
+    zombies.forEach(zombie => {
+      zombie.update(player, zombies)
+      zombie.render(char, frame)
+    })    
+  
+    player.update()
+    player.render(char)
+  
+    if(frame % 400 == 0) {
+      zombies.push(new Zombie(player))
+    }
+  }
+  
+  animate(update)
