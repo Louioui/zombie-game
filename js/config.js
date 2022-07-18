a1efbb0ea069792697940d1df9b3ba54b1e9b592
luis-pacheco <luispacheco13a@gmail.com>
2022-07-18 00:52:24 -0700

    config to alter the behavior

---
+++ js/config.js
@@ -0,0 +1,13 @@
+let width, height;
+
+const resizeConfig = ()=> {
+    width =window.innerWidth
+    height = window.innerHeight
+}
+resizeConfig()
+
+export{
+    width,
+    height,
+    resizeConfig
+}
